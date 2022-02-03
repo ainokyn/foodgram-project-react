99c52c95875e4daa6f802cf4ac6320934dbd124d
Alexandra <aleksandra.alexand2017@yandex.ru>
2022-02-03 22:52:38 +0300

    03.02.2022

--- backend/api/filter.py
+++ backend/api/filter.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django_filters import rest_framework as filters
 from django_filters.fields import CSVWidget
-
 from app.models import Download, Favorite, Recipe, Tag
 
 User = get_user_model()

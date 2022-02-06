088b0191e23276cf8e79a34ac1c9d75b4cd4c2ee
ainokyn <86744194+ainokyn@users.noreply.github.com>
2022-02-07 02:56:45 +0300

    Update filter.py

--- backend/api/filter.py
+++ backend/api/filter.py
@@ -6,30 +6,29 @@ User = get_user_model()
 
 
 class FilterForRecipeFilter(filters.FilterSet):
-    fovorite = filters.BooleanFilter(
-        field_name='is_favorited',
-        method='get_is_favorited')
-    download = filters.BooleanFilter(
-        field_name='is_in_shopping_cart',
-        method='get_is_in_shopping_cart')
-    tags = filters.ModelMultipleChoiceFilter(
+    tags = filters.filters.ModelMultipleChoiceFilter(
         queryset=Tag.objects.all(),
-        field_name='tags__slug', to_field_name='slug',
+        field_name='tags__slug',
+        to_field_name='slug',
+    )
+    download = filters.BooleanFilter(
+        field_name='download', method='filter'
+    )
+    fovorite = filters.BooleanFilter(
+        field_name='fovorite', method='filter'
     )
 
+    def filter(self, queryset, name, value):
+        if name == 'download' and value:
+            queryset = queryset.filter(
+                download__user=self.request.user
+            )
+        if name == 'fovorite' and value:
+            queryset = queryset.filter(
+                fovorite__user=self.request.user
+            )
+        return queryset
+
     class Meta:
-        fields = (
-            'fovorite',
-            'download',
-            'tags'
-        )
-
-    def get_is_favorited(self, queryset, name, value):
-        if value:
-            Recipe.objects.filter(fovorite__user=self.request.user)
-        return Recipe.objects.all()
-
-    def get_is_in_shopping_cart(self, queryset, name, value):
-        if value:
-            return Recipe.objects.filter(download__user=self.request.user)
-        return Recipe.objects.all()
+        model = Recipe
+        fields = ['tags', 'download', 'fovorite']

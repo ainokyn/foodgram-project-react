5df341645bb754133c1042a9c0c73d37fd157a49
ainokyn <86744194+ainokyn@users.noreply.github.com>
2022-02-08 01:09:34 +0300

    Update permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,17 +3,14 @@ from rest_framework import permissions, status
 
 class AnonymAdminAuthor(permissions.BasePermission):
     message = status.HTTP_403_FORBIDDEN
-    edit_methods = ("PUT", "PATCH", "DELETE",)
+    edit_methods = ("PATCH", "DELETE",)
 
     def has_object_permission(self, request, view, obj):
-        user = request.user
-        if request.method == "POST":
-            return user == user.is_authenticated
         if request.method in permissions.SAFE_METHODS:
             return True
         if request.method in self.edit_methods:
             return (
-                user.is_admin()
-                or user == obj.author
+                request.user.is_admin()
+                or request.user == obj.author
             )
         return False
